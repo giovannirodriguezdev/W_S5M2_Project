1f797254721bc6f5c4b596b935f98d732f9a6fdc
Giovanni Rodriguez <giothedesigner@gmail.com>
2025-02-12 13:14:17 -0800

    finished task 3 - arrow keys are functioning

--- frontend/index.js
+++ frontend/index.js
@@ -37,6 +37,10 @@ function moduleProject2() {
       row.appendChild(square)
       square.addEventListener('click', () => {
         // 👉 TASK 2 - Use a click handler to target a square 👈
+        if (!square.classList.contains("targeted")) {
+          document.querySelector(".targeted").classList.remove("targeted");
+          square.classList.add("targeted");
+        }
       })
     }
   }
@@ -65,6 +69,36 @@ function moduleProject2() {
 
   document.addEventListener('keydown', evt => {
     // 👉 TASK 3 - Use the arrow keys to highlight a new square 👈
+    let isUp = evt.key === keys.up;
+    let isDown = evt.key === keys.down;
+    let isLeft = evt.key === keys.left;
+    let isRight = evt.key === keys.right;
+
+    let target = document.querySelector(".targeted");
+
+    if (isUp) {
+      if (target.parentElement.previousElementSibling) {
+        let index = Array.from(target.parentElement.children).indexOf(target);
+        target.classList.remove("targeted");
+        target.parentElement.previousElementSibling.children[index].classList.add("targeted");
+      }
+    } else if (isDown) {
+      if (target.parentElement.nextElementSibling) {
+        let index = Array.from(target.parentElement.children).indexOf(target);
+        target.classList.remove("targeted");
+        target.parentElement.nextElementSibling.children[index].classList.add("targeted");
+      }
+    } else if (isLeft) {
+      if (target.previousElementSibling) {
+        target.classList.remove('targeted');
+        target.previousElementSibling.classList.add('targeted');
+      }
+    } else if(isRight) {
+      if (target.nextElementSibling) {
+        target.classList.remove('targeted');
+        target.nextElementSibling.classList.add('targeted');
+      }
+    }
 
     // 👉 TASK 4 - Use the space bar to exterminate a mosquito 👈
 
