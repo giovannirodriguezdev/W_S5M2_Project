1d27079bbfe188af95261b327a8cdb5fc99b3caf
Giovanni Rodriguez <giothedesigner@gmail.com>
2025-02-12 14:39:13 -0800

    finished project - working game

--- frontend/index.js
+++ frontend/index.js
@@ -73,6 +73,7 @@ function moduleProject2() {
     let isDown = evt.key === keys.down;
     let isLeft = evt.key === keys.left;
     let isRight = evt.key === keys.right;
+    let isSpacebar = evt.key === keys.space;
 
     let target = document.querySelector(".targeted");
 
@@ -101,8 +102,29 @@ function moduleProject2() {
     }
 
     // 👉 TASK 4 - Use the space bar to exterminate a mosquito 👈
+    else if (isSpacebar) {
+      let mosquito = target.firstChild;
 
-    // 👉 TASK 5 - End the game 👈
+      if (mosquito && mosquito.dataset.status === "alive") {
+        mosquito.dataset.status = "dead";
+        mosquito.parentElement.style.backgroundColor = "red";
+      }
+      // 👉 TASK 5 - End the game 👈
+      let liveMosquitos = document.querySelectorAll("[data-status=alive]")
+      if (!liveMosquitos.length) {
+        let elapsed = getTimeElapsed()
+        document.querySelector("p.info").textContent = `Extermination completed in ${elapsed / 1000} seconds!`;
+
+        let restartBtn = document.createElement("button");
+        restartBtn.textContent = "Restart";
+        restartBtn.addEventListener("click", () => {
+          location.reload();
+        })
+        document.querySelector("h2").insertAdjacentElement("beforeend", restartBtn);
+      }
+    }
+
+    
   })
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
